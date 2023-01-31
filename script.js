d1d55c64f35d481142ae3f2fe3f76cc3485d6eb1
Bahare <itxbb09@gmail.com>
2023-01-31 09:37:33 +0000

    Level200 Done

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
 //You can edit ALL of the code here
+const rootElem = document.getElementById("root");
+let searchInput = document.createElement("input");
+searchInput.id = "searchInputId";
+searchInput.placeholder = "Search..";
+rootElem.appendChild(searchInput);
+let searchDisplay = document.createElement("span");
+searchDisplay.id="searchDisplayId";
+rootElem.appendChild(searchDisplay);
+
+
+//function will call when user start typing in a search box
+searchInput.addEventListener("keyup", (event) => {
+  const searchWord = event.target.value.toLowerCase();
+  const allEpisodes = getAllEpisodes();
+  const filterEpisode = allEpisodes.filter(
+    (episode) =>
+      episode.name.toLowerCase().includes(searchWord) ||
+      episode.summary.toLowerCase().includes(searchWord)
+  );
+  searchDisplay.innerText = `Displaying ${filterEpisode.length} / ${allEpisodes.length} episodes`;
+});
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  searchDisplay.innerText = `Displaying ${allEpisodes.length} / ${allEpisodes.length} episodes`;
 }
 
 function makePageForEpisodes(episodeList) {
-  //const rootElem = document.getElementById("root");
-  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  return episodeList.map((episode) => {
-    let SENumber = `S0${episode.season}E0${episode.number}`;
+  let cardsContainer = document.createElement("div");
+  rootElem.appendChild(cardsContainer);
+  cardsContainer.id = "cardsContainer";
+
+  return episodeList.forEach((episode) => {
+    let episodeCode = `S0${episode.season}E0${episode.number}`;
     //create a div for card
     let cardDiv = document.createElement("div");
     cardDiv.id = "cardId";
-    const rootElem = document.getElementById("root");
-    rootElem.appendChild(cardDiv);
-    //create tag img
-    let image = document.createElement("img");
-    image.id = "imageId";
-    image.src = episode.image.medium;
-    cardDiv.appendChild(image);
-    //create tag h3 for name and number
-    let cardName = document.createElement("h3");
-    cardName.textContent = `${episode.name} - ${SENumber}`;
-    cardDiv.appendChild(cardName);
+    cardsContainer.appendChild(cardDiv);
+    //create img tag
+    let epImage = document.createElement("img");
+    epImage.id = "epImageId";
+    epImage.src = episode.image.medium;
+    cardDiv.appendChild(epImage);
+    //create h3 tag for name and number
+    let epName = document.createElement("h3");
+    epName.textContent = `${episode.name} - ${episodeCode}`;
+    cardDiv.appendChild(epName);
     //create tag p for summary
-    let cardSummary = document.createElement("p");
-    cardSummary.textContent = episode.summary;
-    cardDiv.appendChild(cardSummary);
+    let epSummary = document.createElement("p");
+    epSummary.innerHTML = episode.summary;
+    cardDiv.appendChild(epSummary);
   });
 }
 
