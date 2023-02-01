53e34b1fec5c3a76cfca9c3a71cc9d57aa028d08
Bahare <itxbb09@gmail.com>
2023-02-01 21:28:59 +0000

    Level300 Done

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //You can edit ALL of the code here
 const rootElem = document.getElementById("root");
+//create select-box
+const selectBox = document.createElement("select");
+selectBox.id = "selectBox";
+rootElem.appendChild(selectBox);
 //create input box for search
 let searchInput = document.createElement("input");
 searchInput.id = "searchInputId";
@@ -14,51 +18,89 @@ let cardsContainer = document.createElement("div");
 rootElem.appendChild(cardsContainer);
 cardsContainer.id = "cardsContainer";
 
-//function will call when user start typing in a search box
-searchInput.addEventListener("keyup", (event) => {
-  const searchWord = event.target.value.toLowerCase();
-  const allEpisodes = getAllEpisodes();
-  const filterEpisode = allEpisodes.filter(
-    (episode) =>
-      episode.name.toLowerCase().includes(searchWord) ||
-      episode.summary.toLowerCase().includes(searchWord)
-  );
-  //call the function to make a new page for filter episodes
-  makePageForEpisodes(filterEpisode);
-  //display the search result
-  searchDisplay.innerText = `Displaying ${filterEpisode.length} / ${allEpisodes.length} episodes`;
-});
-
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
   searchDisplay.innerText = `Displaying ${allEpisodes.length} / ${allEpisodes.length} episodes`;
+  makeSelectBox(allEpisodes);
+}
+
+//create episodeTitle
+function episodeTitle(episode) {
+  return `${episode.name} - S${episode.season
+    .toString()
+    .padStart(2, 0)}E${episode.number.toString().padStart(2, 0)}`;
 }
 
 function makePageForEpisodes(episodeList) {
   //reset the page
   cardsContainer.innerHTML = "";
-  
+
   return episodeList.forEach((episode) => {
-    let episodeCode = `S0${episode.season}E0${episode.number}`;
     //create a div for card
     let cardDiv = document.createElement("div");
     cardDiv.id = "cardId";
     cardsContainer.appendChild(cardDiv);
+    //create h3 tag for name and number
+    let epTitle = document.createElement("h3");
+    epTitle.id = "epTitle";
+    epTitle.innerText = episodeTitle(episode);
+    cardDiv.appendChild(epTitle);
+
     //create img tag
     let epImage = document.createElement("img");
     epImage.id = "epImageId";
     epImage.src = episode.image.medium;
     cardDiv.appendChild(epImage);
-    //create h3 tag for name and number
-    let epName = document.createElement("h3");
-    epName.textContent = `${episode.name} - ${episodeCode}`;
-    cardDiv.appendChild(epName);
     //create tag p for summary
-    let epSummary = document.createElement("p");
-    epSummary.innerHTML = episode.summary;
-    cardDiv.appendChild(epSummary);
+    //let epSummary = document.createElement("p");
+    //epSummary.innerText = episode.summary;
+    cardDiv.innerHTML += episode.summary;
   });
 }
+
+//function will call when user start typing in a search box
+searchInput.addEventListener("keyup", (event) => {
+  const searchWord = event.target.value.toLowerCase();
+  const allEpisodes = getAllEpisodes();
+  const filterEpisode = allEpisodes.filter((episode) => {
+    return (
+      episode.name.toLowerCase().includes(searchWord) ||
+      episode.summary.toLowerCase().includes(searchWord)
+    );
+  });
+  //call the function to make a new page for filter episodes
+  makePageForEpisodes(filterEpisode);
+  //display the search result
+  searchDisplay.innerText = `Displaying ${filterEpisode.length} / ${allEpisodes.length} episodes`;
+});
+// function for create select options
+function makeSelectBox(episodeList) {
+  let option = document.createElement("option");
+  option.innerText = "All Episodes";
+  document.getElementById("selectBox").appendChild(option);
+  episodeList.forEach((episode) => {
+    let option = document.createElement("option");
+    option.innerText = episodeTitle(episode);
+    document.getElementById("selectBox").appendChild(option);
+  });
+}
+selectBox.addEventListener("change", (event) => {
+  const selectedEpisode = event.target.value;
+  let allEpisodes = getAllEpisodes();
+
+  if (selectedEpisode === "All Episodes") {
+    return makePageForEpisodes(allEpisodes);
+  } else {
+    const selectEp = allEpisodes.filter((episode) => {
+      return episodeTitle(episode) === selectedEpisode;
+    });
+    console.log(selectEp);
+
+    makePageForEpisodes(selectEp);
+  }
+});
+
+
 
 window.onload = setup;
